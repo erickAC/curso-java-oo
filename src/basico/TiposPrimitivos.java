ec5325da071d64b99b7c6929018c392d1831747e
Erick Andrade <erick.cirqueira@pontta.com>
2026-04-25 08:22:42 -0300

    feat: adicionado arquivos de tipos primitivos e tipos wrapper

---
+++ src/basico/TiposPrimitivos.java
@@ -0,0 +1,19 @@
+package basico;
+
+public class TiposPrimitivos {
+
+  public static void main(String[] args) {
+
+    // tipos primitivos não permitem valores nulos
+    int idade = 10;
+    char letra = 'E';
+    double salario = 1000.50;
+    float imposto = 10.5f;
+    boolean ativo = true;
+
+    // Para atribuir um valor a uma variavel devemos sempre informar o tipo primeiro, como em C.
+    // tipo nome_variavel = valor;
+
+  }
+
+}
---
+++ src/basico/TiposWrapper.java
@@ -0,0 +1,17 @@
+package basico;
+
+public class TiposWrapper {
+
+  public static void main(String[] args) {
+
+    // Agora podemos usar valores nulos
+    Integer idade = null;
+    idade = 23;
+    String nome = "Erick";
+    Float salario = 1000.50f;
+    Double imposto = 10.5;
+    Boolean ativo = true;
+
+  }
+
+}
